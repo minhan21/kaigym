fe3eac20f53e9a4804b9f730d38fc5c8088a5db6
An Tran <antran@Ans-Mac-Studio.local>
2024-05-12 13:12:44 +0700

    logout feature

--- navigation/MainNavigator.tsx
+++ navigation/MainNavigator.tsx
@@ -1,12 +1,30 @@
 import HomeScreen from "../screens/MainScreen/HomeScreen";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SettingScreen from "../screens/MainScreen/SettingScreen";
+import Icon from "@components/Icon";
+import Colors from "@constants/Colors";
 
 const Tab = createBottomTabNavigator();
 
 const MainNavigator = () => {
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarActiveTintColor: Colors.light.primaryColor,
+        tabBarInactiveTintColor: Colors.light.gray1,
+        tabBarIcon: ({ focused, color, size }) => {
+          // Use the Image component to display the icon
+          return (
+            <Icon
+              focused={focused}
+              icon={route.name}
+              size={size}
+              color={color}
+            />
+          );
+        },
+      })}
+    >
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Settings" component={SettingScreen} />
     </Tab.Navigator>
--- screens/MainScreen/SettingScreen/index.tsx
+++ screens/MainScreen/SettingScreen/index.tsx
@@ -1,10 +1,38 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import { ActivityIndicator, TouchableOpacity } from "react-native";
+import Block from "@components/BaseComponent/Block";
+import Typography from "@components/BaseComponent/Text";
+import Icon from "@components/Icon";
+import Colors from "@constants/Colors";
+import { NavigationTypes } from "@navigation/navigationTypes";
+import { useNavigation } from "@react-navigation/native";
+import { StackNavigationProp } from "@react-navigation/stack";
+import { logoutUser } from "../../Authentication/authenticateCollection";
 
 const SettingScreen = () => {
+  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation<StackNavigationProp<NavigationTypes>>();
+
+  const navigateToLoginScreen = () => {
+    navigation.navigate("Login");
+  };
+  const handleLogout = async () => {
+    setLoading(true); // Start loading
+    const result = await logoutUser();
+    if (result.success) {
+      navigateToLoginScreen();
+    }
+    setLoading(false); // Stop loading
+  };
   return (
-    <View>
-      <Text>SettingScreen</Text>
-    </View>
+    <Block radius={0} flex backgroundColor={Colors.light.white}>
+      <TouchableOpacity onPress={handleLogout}>
+        <Block margin={8} row alignCenter>
+          {loading ? <ActivityIndicator /> : <Icon icon="LogOut" size={18} />}
+          <Typography marginLeft={8}>Log out</Typography>
+        </Block>
+      </TouchableOpacity>
+    </Block>
   );
 };
 
